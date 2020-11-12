11c66aa4fea81e80a46a0849b3ff141095f0abcd
John <jpowell575@gmail.com>
2020-11-11 22:42:29 -0500

    Finished lab

---
+++ main.js
@@ -0,0 +1,15 @@
+const num = 35; //35 is a constant
+console.log(num);
+
+let variable = 4; //4 is a variable
+console.log(variable);
+
+let multiplication = num * variable; //35 times 4
+
+console.log(11 + 43); //Addition of numbers
+
+console.log('The sum'); //1st string
+let string2 = 'of both parts'; //2nd string
+console.log('The sum ' + string2); //Addition of strings
+
+console.log(num * variable); //Constant multiplied by a variable
